472157d40eee8fb56f86ad67e1584d287b55d675
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2023-02-15 09:54:03 +0530

    minute changes in courseInfo

--- app/src/main/java/com/axyz/upasthithshishya/courses/CourseInfo.kt
+++ app/src/main/java/com/axyz/upasthithshishya/courses/CourseInfo.kt
@@ -18,15 +18,18 @@ class CourseInfo : AppCompatActivity() {
         val department = findViewById<TextView>(R.id.departmentTextView)
         val semester = findViewById<TextView>(R.id.semesterTextView)
         val attendance = findViewById<LinearLayout>(R.id.attendanceView)
+        val description = findViewById<TextView>(R.id.descriptionTextView)
         val name: String? = intent.getStringExtra("Course Name")
         val code: String? = intent.getStringExtra("Course Code")
         val departmentName: String? = intent.getStringExtra("Department")
         val semesterNo: String? = intent.getStringExtra("Semester")
+        val courseDescription: String? = intent.getStringExtra("Description")
         courseName.text = name
         courseHeading.text = name
         courseCode.text = code
         department.text = departmentName
         semester.text = semesterNo
+        description.text = courseDescription
         attendance.setOnClickListener{
             startActivity(Intent(this, ShowAttendanceActivity::class.java))
         }
