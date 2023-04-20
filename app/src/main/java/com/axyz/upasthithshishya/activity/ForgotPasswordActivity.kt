7b776f81bfe341de0df34e9cd7f4f84321cd61ae
Vidit Jain <rsvr516@gmail.com>
2023-04-20 05:54:52 +0530

    Onboarding done and some changes in forgot,verify,update pass and some minors

---
+++ app/src/main/java/com/axyz/upasthithshishya/activity/ForgotPasswordActivity.kt
@@ -0,0 +1,18 @@
+package com.axyz.upasthithshishya.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.axyz.upasthithshishya.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+        val request_change = findViewById<AppCompatButton>(R.id.btn_send)
+        request_change.setOnClickListener {
+            startActivity(Intent(this, VerifyPassChangeActivity::class.java))
+        }
+    }
+}
